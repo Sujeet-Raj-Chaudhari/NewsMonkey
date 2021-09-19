563baa35768fd0573cbf091b0f2d4a87d5e4bf23
Sujeet Raj Chaudhari <61381167+Sujeet-Raj-Chaudhari@users.noreply.github.com>
2021-09-19 15:46:55 +0530

    AfterNewsCard

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,17 +1,44 @@
-import React, { Component } from 'react'
-import NewsItems from './NewsItems'
+import React, { Component } from "react";
+import NewsItems from "./NewsItems";
 
 export class News extends Component {
-    render() {
-        return (
-            <div>
-                Newses
-                <NewsItems/>
+  render() {
+    return (
+      <div>
+        <div className="container my-3">
+            <h2>
+                <i>News Monkey's - Top HeadLines</i>
+            </h2>
+            <div className="row">
+                <div className="col md-3">
+                    <NewsItems title="NewsHeadline" description="description" />
+                </div>
+                <div className="col md-3">
+                    <NewsItems title="NewsHeadline" description="description" />
+                </div>
+                <div className="col md3-">
+                    <NewsItems title="NewsHeadline" description="description" />
+                </div>
+                <div className="col md-3">
+                    <NewsItems title="NewsHeadline" description="description" />
+                </div>
+                <div className="col md-3">
+                    <NewsItems title="NewsHeadline" description="description" />
+                </div>
+                <div className="col md-3">
+                    <NewsItems title="NewsHeadline" description="description" />
+                </div>
+                <div className="col md-3">
+                    <NewsItems title="NewsHeadline" description="description" />
+                </div>
+                <div className="col md-3">
+                    <NewsItems title="NewsHeadline" description="description" />
+                </div>
             </div>
-        )
-    }
+        </div>
+      </div>
+    );
+  }
 }
 
-export default News
-
-
+export default News;
--- src/Components/NewsItems.js
+++ src/Components/NewsItems.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react'
 
 export class NewsItems extends Component {
     render() {
+        let {title, description} = this.props;
         return (
             <div>
-                NewsItemColumn
+                <div className ="card" style={{width : "18rem"}}>
+                    <img src="..." className ="card-img-top" alt="..."/>
+                    <div className ="card-body">
+                        <h5 className ="card-title">{title}</h5>
+                        <p className ="card-text">{description}</p>
+                        <a href="/news" className ="btn btn-primary">Read Full News</a>
+                    </div>
+                </div>
             </div>
         )
     }
