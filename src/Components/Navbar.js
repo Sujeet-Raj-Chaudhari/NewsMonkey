b34a3080d463fcd990046580d6aa307c62d73cea
Sujeet Raj Chaudhari <61381167+Sujeet-Raj-Chaudhari@users.noreply.github.com>
2021-09-28 21:51:32 +0530

    Fetching News By Categories

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,7 +4,7 @@ export class Navbar extends Component {
     render() {
         return (
             <div>
-                <nav className ="navbar navbar-expand-lg navbar-light bg-light">
+                <nav className ="navbar navbar-expand-lg navbar-dark bg-dark">
                     <div className ="container-fluid">
                         <a className ="navbar-brand" href="/">Navbar</a>
                         <button className ="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -13,11 +13,26 @@ export class Navbar extends Component {
                         <div className ="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul className ="navbar-nav me-auto mb-2 mb-lg-0">
                                 <li className ="nav-item">
-                                <a className ="nav-link active" aria-current="page" href="/">Home</a>
+                                <a className ="nav-link" aria-current="page" href="/">Home</a>
                                 </li>
                                 <li className ="nav-item">
-                                <a className ="nav-link" href="/">Link</a>
+                                <a className ="nav-link" href="/">About</a>
                                 </li>
+                                <li className ="nav-item">
+                                <a className ="nav-link active" aria-current="page" href="/">business</a></li>
+                                <li className ="nav-item">
+                                <a className ="nav-link active" aria-current="page" href="/">entertainment</a></li>
+                                <li className ="nav-item">
+                                <a className ="nav-link active" aria-current="page" href="/">sports</a></li>
+                                <li className ="nav-item">
+                                <a className ="nav-link active" aria-current="page" href="/">general</a></li>
+                                <li className ="nav-item">
+                                <a className ="nav-link active" aria-current="page" href="/">health</a></li>
+                                <li className ="nav-item">
+                                <a className ="nav-link active" aria-current="page" href="/">science</a></li>
+                                <li className ="nav-item">
+                                <a className ="nav-link active" aria-current="page" href="/">technology</a></li>
+
                             </ul>
                         </div>
                     </div>
--- src/Components/NewsItems.js
+++ src/Components/NewsItems.js
@@ -5,12 +5,12 @@ export class NewsItems extends Component {
         let {title, description, imageUrl, newsUrl} = this.props;
         return (
             <div>
-                <div className ="card" style={{width : "18rem"}}>
+                <div className ="card" style={{width : "25rem", height: "502px"}}>
                     <img style = {{height : "286px"}}src={imageUrl} className ="card-img-top" alt="..."/>
                     <div className ="card-body">
                         <h5 className ="card-title">{title}</h5>
                         <p className ="card-text">{description}</p>
-                        <a href={newsUrl} className ="btn btn-primary">Read Full News</a>
+                        <a href={newsUrl} className ="btn btn-dark">Read Full News</a>
                     </div>
                 </div>
             </div>
