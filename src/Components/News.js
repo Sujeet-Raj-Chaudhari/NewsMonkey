8fffa5b8299e29793ab1a7872e729c8de3676504
Sujeet Raj Chaudhari <61381167+Sujeet-Raj-Chaudhari@users.noreply.github.com>
2021-10-20 19:07:27 +0530

    Finalized2

--- src/Components/News.js
+++ src/Components/News.js
@@ -6,96 +6,107 @@ import PropTypes from "prop-types";
 import InfiniteScroll from "react-infinite-scroll-component";
 
 const News = (props) => {
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [totalResults, setTotalResults] = useState(0);
 
-  const[articles, setArticles] = useState([])
-  const[loading, setLoading] = useState(true)
-  const[page, setPage] = useState(1)
-  const[totalArticles, setTotalArticles] = useState(0)
-
-  const capitalizeFirstLetter = (string) =>{
+  const capitalizeFirstLetter = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
-  }
+  };
 
   const updateNews = async () => {
     props.setProgress(10);
     const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
-    setLoading(true)
+    setLoading(true);
     let data = await fetch(url);
-    props.setProgress(40);
-    let parseData = await data.json();
+    props.setProgress(30);
+    let parsedData = await data.json();
     props.setProgress(70);
-
-    setArticles(parseData.articles)
-    setTotalArticles(parseData.totalArticles)
-    setLoading(false)
+    setArticles(parsedData.articles);
+    setTotalResults(parsedData.totalResults);
+    setLoading(false);
     props.setProgress(100);
-    
-  }
+  };
 
   useEffect(() => {
     document.title = `${capitalizeFirstLetter(props.category)} - NewsMonkey`;
     updateNews();
-    //eslint-disable-next-line
-  }, [])
+    // eslint-disable-next-line
+  }, []);
 
-
-  const fetchMoreData =async () =>{
-    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page + 1}&pageSize=${props.pageSize}`;
-    setPage(page + 1)
+  const fetchMoreData = async () => {
+    const url = `https://newsapi.org/v2/top-headlines?country=${
+      props.country
+    }&category=${props.category}&apiKey=${props.apiKey}&page=${
+      page + 1
+    }&pageSize=${props.pageSize}`;
+    setPage(page + 1);
     let data = await fetch(url);
-    let parseData = await data.json();
-
-    setArticles(articles.concat(parseData.articles))
-    setTotalArticles(parseData.totalArticles)
+    let parsedData = await data.json();
+    setArticles(articles.concat(parsedData.articles));
+    setTotalResults(parsedData.totalResults);
   };
 
-    return (
-      <>
-          <h1 className="text-center" style={{ margin: "40px 0px" , color: "white", marginTop: "50px" }}>
-            <i>News Monkey's - Top HeadLines from {capitalizeFirstLetter(props.category)}</i>
-          </h1>
-          {loading && <Spinner />}
+  return (
+    <>
+      <h1
+        className="text-center"
+        style={{ margin: "40px 0px", color: "white", marginTop: "50px" }}
+      >
+        <i>
+          News Monkey's - Top HeadLines from{" "}
+          {capitalizeFirstLetter(props.category)}
+        </i>
+      </h1>
+      {loading && <Spinner />}
 
-          <InfiniteScroll
-          dataLength={articles.length}
-          next={fetchMoreData}
-          hasMore={articles.length !== totalArticles}
-          loader={<Spinner/>}
-          >
-
-          <div className="container">
+      <InfiniteScroll
+        dataLength={articles.length}
+        next={fetchMoreData}
+        hasMore={articles.length !== totalResults}
+        loader={<Spinner />}
+      >
+        <div className="container">
           <div className="row">
             {articles.map((element) => {
-                return (
-                  <div className="col-md-4 my-3" key={element.url}>
-                    <NewsItems
-                      title={element.title ? element.title.slice(0, 45) : " "}
-                      description={element.description?element.description.slice(0, 88):" "}
-                      imageUrl={element.urlToImage? element.urlToImage:"https://unblast.com/wp-content/uploads/2020/04/404-Page-Illustration.jpg"}
-                      newsUrl={element.url}
-                      author={element.author}
-                      date={element.publishedAt}
-                    />
-                  </div>
-                );
-              })}
+              return (
+                <div className="col-md-4 my-3" key={element.url}>
+                  <NewsItems
+                    title={element.title ? element.title.slice(0, 45) : " "}
+                    description={
+                      element.description
+                        ? element.description.slice(0, 88)
+                        : " "
+                    }
+                    imageUrl={
+                      element.urlToImage
+                        ? element.urlToImage
+                        : "https://unblast.com/wp-content/uploads/2020/04/404-Page-Illustration.jpg"
+                    }
+                    newsUrl={element.url}
+                    author={element.author}
+                    date={element.publishedAt}
+                  />
+                </div>
+              );
+            })}
           </div>
-          </div>
-
-          </InfiniteScroll>
-      </>
-    );
-}
+        </div>
+      </InfiniteScroll>
+    </>
+  );
+};
 
 News.defaultProps = {
   country: "in",
   pageSize: 6,
-}
+};
 
 News.propTypes = {
   country: PropTypes.string,
   pageSize: PropTypes.number,
   category: PropTypes.string,
-}
+};
 
 export default News;
